2d59a137dbf05fa7187e6d6303ef1d12c3afce7b
Joel Wood <joelmwood@live.com>
2022-12-27 12:37:24 -0600

    Finished Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = "";
+    let passedString = string.toLowerCase();
+    passedString = passedString.replace(/[^a-zA-Z0-9 ]/g, '');
+    passedString = passedString.replace(/\s/g, '');
+    if(passedString.length === 1) return true;
+    for(i=passedString.length-1; i >= 0; i--){
+        newString += passedString[i];
+    }
+    if (newString == passedString) {
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
