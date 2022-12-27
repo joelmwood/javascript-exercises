3fc1289f46d4f08bbb091b5452fb475116de7533
Joel Wood <joelmwood@live.com>
2022-12-27 13:08:13 -0600

    Finished Fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let answer = 0;
+    let tempNum = parseInt(num);   
+    
+    if (tempNum === 0) return 0;
+    if (tempNum === 1) return 1;
+    if (tempNum === 2) return 1;
+    if(tempNum < 0) return "OOPS";
+    
+    let newArray = [0, 1, 1];
 
+    for(let i = 3; i < tempNum + 1; i++){
+        newArray.push(newArray[i-1] + newArray[i-2]);
+        // console.log(newArray);
+    };
+    answer = newArray[newArray.length-1];
+    return answer;
 };
 
 // Do not edit below this line
