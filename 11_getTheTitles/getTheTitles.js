24a9021c10660eb2c7b63cb90a2960f178d14e6c
Joel Wood <joelmwood@live.com>
2022-12-27 13:31:21 -0600

    Finished getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(passedArray) {
+    return passedArray.map(book => book.title)
+    //.map creates a new array  
 };
 
 // Do not edit below this line
